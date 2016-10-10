98094aaaaaea56a44de6488b9c1045ee0ccf7655
Stephan Westen <stephanwesten@gmail.com>
2016-10-10 14:58:16 +0200

    gbp conversion added

--- www/js/conversionService.js
+++ www/js/conversionService.js
@@ -10,10 +10,11 @@ angular.module('calcworks.services')
         // we use the deferred pattern to make the sync operations also to be treated async
         var deferred = $q.defer();
         var conversionCalc = sheet.createNewCalculation();
-        var processExchangeRateResponseEURtoUSD = function(rate) {
-                var rateCalc = sheet.searchCalculation('euro to usd rate');
+        var processExchangeRateResponseToCurrency = function(currency, rate) {
+                var rateName = 'euro to ' + currency + ' rate'
+                var rateCalc = sheet.searchCalculation(rateName);
                 if (!rateCalc) {
-                    rateCalc = sheet.createNewCalculation('euro to usd rate');
+                    rateCalc = sheet.createNewCalculation(rateName);
                     rateCalc.expression = [Number(rate)];
                     rateCalc.result = Number(rate);
                     sheet.addCalculation(rateCalc);
@@ -21,10 +22,11 @@ angular.module('calcworks.services')
                 conversionCalc.expression = [ calc, 'x', rateCalc ];
                 deferred.resolve(conversionCalc);
             };
-        var processExchangeRateResponseUSDtoEUR = function(rate) {
-                var rateCalc = sheet.searchCalculation('usd to euro rate');
+        var processExchangeRateResponseToEUR = function(currency, rate) {
+                var rateName = currency + ' to euro rate';
+                var rateCalc = sheet.searchCalculation(rateName);
                 if (!rateCalc) {
-                    rateCalc = sheet.createNewCalculation('usd to euro rate');
+                    rateCalc = sheet.createNewCalculation(rateName);
                     var inverseRate = 1 / rate;
                     rateCalc.expression = [Number(inverseRate)];
                     rateCalc.result = Number(inverseRate);
@@ -36,11 +38,19 @@ angular.module('calcworks.services')
         if (operator === 'usd-to-eur') {
             $rootScope.showWaitingIcon();
             conversionCalc.name = calc.name + ' in euros';
-            this.getUSD_EUR(processExchangeRateResponseUSDtoEUR);
+            this.getExchangeRate('usd', processExchangeRateResponseToEUR);
         } else if (operator === 'eur-to-usd') {
             $rootScope.showWaitingIcon();
             conversionCalc.name = calc.name + ' in usd';
-            this.getUSD_EUR(processExchangeRateResponseEURtoUSD);
+            this.getExchangeRate('usd', processExchangeRateResponseToCurrency);
+        } else if (operator === 'eur-to-gbp') {
+            $rootScope.showWaitingIcon();
+            conversionCalc.name = calc.name + ' in gbp';
+            this.getExchangeRate('gbp', processExchangeRateResponseToCurrency);
+        } else if (operator === 'gbp-to-eur') {
+            $rootScope.showWaitingIcon();
+            conversionCalc.name = calc.name + ' in euros';
+            this.getExchangeRate('gbp', processExchangeRateResponseToEUR);
         } else {
             if (operator === 'inch-to-centimeters') {
                 conversionCalc.name = calc.name + ' to centimeters';
@@ -73,6 +83,39 @@ angular.module('calcworks.services')
         return deferred.promise;
     }
 
+
+    // D: daily,  SP00.A is een code voor exchange rate service
+    var ecbURLs = {
+      'usd' : 'https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.USD.EUR.SP00.A?lastNObservations=1',
+      'gbp' : 'https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.GBP.EUR.SP00.A?lastNObservations=1'
+    };
+
+    this.getExchangeRate = function(currency, callback) {
+        var processError = function(reason) {
+            $rootScope.hideWaitingIcon();
+            $ionicPopup.alert({
+              title: 'Error',
+              template: 'Failed to retrieve exchange rate'
+            });
+
+        }
+
+        var processResponse = function(response) {
+            var rate = response.data.dataSets[0].series['0:0:0:0:0'].observations['0'][0];
+            callback(currency, rate);
+            $rootScope.hideWaitingIcon();
+        }
+
+        var request = {
+            method: 'GET',
+            url: ecbURLs[currency],
+            headers: {
+                'Accept'  : 'application/vnd.sdmx.data+json;version=1.0.0-cts',
+            }
+        }
+        $http(request)
+            .then(processResponse, processError);
+    };
 
 
 
